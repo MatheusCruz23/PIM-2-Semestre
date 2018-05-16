e80b0dfc38871876792e9c751688c8233c476bed
MatheusCruz23 <31697408+MatheusCruz23@users.noreply.github.com>
2018-05-16 11:20:38 -0300

    Busca de disciplina
    Faz a busca da disciplina de acordo com a turma selecionada

--- ajax.php
+++ ajax.php
@@ -1,13 +1,15 @@
 <?php 
 require_once("conexao.php");
 
-$query = "SELECT grade.*, disciplina.*, curso.*, turma.*, professor.* FROM grade 
-      INNER JOIN disciplina ON grade.id_disciplina = disciplina.id_disciplina 
-      INNER JOIN curso ON grade.id_curso = curso.id_curso 
-      INNER JOIN turma ON turma.id_curso = curso.id_curso
-      INNER JOIN professor ON grade.id_professor = professor.id_professor
-      WHERE turma.id_turma = {$_GET['id']}
-      ORDER BY nome_disciplina ASC";
+// Recebe o id da turma para fazer a query trazendo a disciplina com o professor
+$query = "SELECT * FROM grade
+      JOIN disciplina ON grade.id_disciplina = disciplina.idDisciplina
+      JOIN curso ON grade.id_curso = curso.idCurso
+      JOIN turma ON curso.idCurso = turma.id_curso
+      JOIN professor ON grade.id_professor = professor.idProfessor
+      WHERE turma.idTurma = {$_GET['id']}
+      ORDER BY nome_disciplina";
+
       $disciplina = mysqli_query($conexao, $query);
       echo '<option value="">Selecione a Disciplina</option>';
       while ($listaDisciplina = mysqli_fetch_assoc($disciplina)) { 
